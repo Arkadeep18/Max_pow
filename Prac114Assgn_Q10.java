b99c87dbf4408896282d1c7ef88ba087c6123936
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-19 22:12:08 +0530

    Add files via upload

---
+++ Prac114Assgn_Q10.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Prac114Assgn_Q10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		int power = 1;
+		
+		while(3*power<=n) {
+			power = power * 3;
+			
+		}
+				
+System.out.println(power);
+	}
+
+}
